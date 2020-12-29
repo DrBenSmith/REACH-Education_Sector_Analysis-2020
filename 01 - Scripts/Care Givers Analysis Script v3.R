1304fe2df4f111fbc35f90b83e823c8ca24f3be0
DrBenSmith <bensmith1@msn.com>
2020-12-29 11:50:19 +0000

    2 scripts added

---
+++ 01 - Scripts/Care Givers Analysis Script v3.R
@@ -0,0 +1,223 @@
+#---------------------------------
+# Education Sector - Care Givers
+# Analysis Script
+#---------------------------------
+# Ben.Smith@reach.initiative.org
+# 07/12/2020
+#-----------
+
+
+# Preamble ----------------------------------------------------------------
+
+library(openxlsx)
+library(dplyr)
+library(srvyr)
+library(butteR)
+library(stringr)
+
+library(illuminate)
+library(AMR)
+library(tidyr)
+
+# Function: Split a vector of strings by "AND" and "&" and trim white space:
+kat_split_by_conjunction = function(string_vector){
+  spit_string_vector = gsub(pattern = c("AND"), replacement = ",", x = string_vector)
+  spit_string_vector = gsub(pattern = c("&"), replacement = ",", x = spit_string_vector)
+  spit_string_vector = gsub(pattern = c(" "), replacement = "", x = spit_string_vector)
+  spit_string_vector = unique(strsplit(x = spit_string_vector, split = ","))
+  return(spit_string_vector)
+}
+
+# Set the directory:
+setwd("C:/Users/Ben SMITH/Documents/Bangladesh/REACH Projects/Projects/Education Sector Assessment/")
+
+# Either create the cleaned data:
+# source("Scripts/Care Givers Cleaning Script v3.R") # This could do with being updated to output CSVs instead of xlxs as loading in data as an xlsx seems to crash the butteR functions. Below, the data has been coppied into csvs manually from the xlsx files.
+
+# Or load it in directly:
+hh <- read.csv(
+  file = "03 - Outputs/02_clean_data/2020_12_15_hh_cleaned_data_with_consent.csv",
+  na.strings = c(""," ","NA","N/A"),
+  stringsAsFactors = FALSE)
+
+indv = read.csv(
+  file = "03 - Outputs/03_composite_indicators_with_data/Individual data - cleaned data with composite indicators - 2020_12_15.csv",
+  na.strings = c(""," ","NA","N/A"),
+  stringsAsFactors = FALSE)
+
+# Read in the Data Analysis Plan:
+DAP <- read.xlsx(xlsxFile = "04 - Other/CAREGIVER-SURVEY_DA instructions_v3.xlsx",
+                 sheet = "Reduced", na.strings = c(""," ","NA","N/A"),
+                 colNames = TRUE, rowNames = FALSE)
+
+# Do you want the results to be collated onto a single page?
+collate = FALSE # or FALSE
+
+# Weightings --------------------------------------------------------------
+  
+  # Households in host community:
+  hh_number <- read.csv("02 - Inputs/Weightings - number of households summary.csv",
+                        na.strings = c(""," ","NA","N/A"))
+  
+  # Add a column into the hh data that you can use to join to the weightings data. 
+  hh <- hh %>% mutate(strata = paste0(group_of_caregivers ,"_", upazila))
+  
+  # Get the number of samples for each hh strata: 
+  sample_frame <- hh %>% group_by(strata) %>% dplyr::summarise(sample_size = n())
+  sample_frame <- left_join(x = sample_frame, y = hh_number)
+  
+  # Create the weighting variables for host communities:
+  sf_with_weights_host <- sample_frame %>% dplyr::filter(Care_Giver == "host_communities")
+  sf_with_weights_host <- sf_with_weights_host %>% dplyr::mutate(
+    sample_global = sum(sample_size),
+    pop_global = sum(Total_HH),
+    survey_weight = (Total_HH/pop_global)/(sample_size/sample_global)
+  )
+  
+  # Create the weighting variables for camps:
+  sf_with_weights_camp <- sample_frame %>% dplyr::filter(Care_Giver == "camps")
+  sf_with_weights_camp <- sf_with_weights_camp %>% dplyr::mutate(
+    sample_global = sum(sample_size),
+    pop_global = sum(Total_HH),
+    survey_weight = (Total_HH/pop_global)/(sample_size/sample_global)
+  )
+  
+  # Join the two above tables together:
+  sample_frame <- bind_rows(sf_with_weights_host, sf_with_weights_camp) 
+  
+  # Write the weightings for reference:
+  write.csv(sample_frame, "03 - Outputs/02_clean_data/weights.csv")
+  
+  
+
+# Add weightings to the data: ---------------------------------------------
+
+  # Crop down the weightings so that they only join what is needed:
+  sample_frame <- sample_frame%>% select(strata, survey_weight)
+  
+  # Join the household data to their weightings:
+  hh_with_weights <- hh %>% left_join(sample_frame)
+  
+  # Join the individual data to weightings using the household data, as this is simplest:
+  indv_with_weights <- indv %>% left_join(hh_with_weights %>% select(X_uuid, strata, survey_weight))
+  
+  
+
+# Create survey objects ---------------------------------------------------
+
+  hh_svy <- as_survey(hh_with_weights, strata = strata, weights = survey_weight)
+  indv_svy <- as_survey(indv_with_weights, strata=strata, weights = survey_weight)
+  
+
+# Define which columns should and should not be analysed ------------------
+
+  hh_cols_not_to_analyse <- c("survey_date", "survey_start", "deviceid", 
+                              "end_survey", "instance_name","end_note", "end",
+                              "audit", "enumerator_id", "kii_id", "int_note", 
+                              "informed_consent", "start","gps_reading",
+                              "X_gps_reading_longitude", "X_gps_reading_altitude", 
+                              "X_gps_reading_precision", "X_gps_reading_latitude",
+                              "X_id", "X_uuid", "X_submission_time", 
+                              "X_validation_status","X_index", "strata", 
+                              "survey_weight")
+  
+  hh_cols_to_analyse <- hh_with_weights %>% select(-ends_with("_other"),
+                                                   -starts_with("other_"),
+                                                   -hh_cols_not_to_analyse) %>% names() 
+  
+  
+  indv_cols_not_to_analyse <- c("strata", "survey_weight","X_index", 
+                                "X_parent_table_name", "X_parent_index", 
+                                "X_submission__id",  "X_uuid", 
+                                "X_submission__submission_time",
+                                "X_submission__validation_status")
+  
+  
+  indv_cols_to_analyse <- indv_with_weights %>% select(-ends_with("_other"),
+                                                   -starts_with("other_"),
+                                                   -indv_cols_not_to_analyse) %>% names() 
+
+
+# Calculate the proportions and means -------------------------------------
+
+  # Create empty lists to take the analysis results if needed:
+  hh_results = list()
+  indv_results = list()
+
+  # Household Analysis:
+  
+  # Find the groups to analyse with:
+  group_names = unique(trimws(unlist(strsplit(x = DAP$Analysis_Level[DAP$Group=="household"],
+                                              split = ","))))
+  
+  # Convert analysis levels that contain "AND"s and "&"s into a list of vectors: 
+  groups = kat_split_by_conjunction(group_names)
+  
+  # Set up a loop to run through the data and fill the list:
+  for(i in 1:length(group_names)){
+    
+    print(paste0(i, ". -------- ", group_names[i], " --------"))
+    
+    if(all(groups[[i]] == "overall")){
+      x = mean_prop_working(design = hh_svy,
+                            list_of_variables = hh_cols_to_analyse)
+      
+    } else {
+      x = mean_prop_working(design = hh_svy,
+                            list_of_variables = hh_cols_to_analyse,
+                            aggregation_level = groups[[i]])
+    }
+    
+    x <- as.data.frame(t(x))
+    colnames(x) <- rep(group_names[i], ncol(x))
+
+      # Assign the result to a tab. Number the tab as some cropped names are identical)
+      hh_results[[paste0(i, "_", substr(x = group_names[i], start=1, stop = 28))]] =  x
+      
+      # Paste the group names into the sheet:
+      if(i == length(group_names)){
+        hh_results[["groupings"]] = group_names
+        hh_results[["input data"]] = hh_svy$variables
+        }
+    
+    
+  }
+  
+  write.xlsx(hh_results,  paste0("03 - Outputs/04_basic_analysis/", str_replace_all(Sys.Date(),"-","_"), "_hh_analysis.xlsx"), row.names = TRUE)
+  
+  
+  # Individual Analysis:
+  
+  # Find the groups to analyse with:
+  group_names = unique(trimws(unlist(strsplit(x = DAP$Analysis_Level[DAP$Group=="individual"],
+                                              split = ","))))
+  
+  # Convert analysis levels that contain "AND"s and "&"s into a list of vectors: 
+  groups = kat_split_by_conjunction(group_names)
+  
+  # Set up a loop to run through the data and fill the list:
+  for(i in 1:length(group_names)){
+    
+    print(paste0(i, ". -------- ", group_names[i], " --------"))
+
+    if(all(groups[[i]] == "overall")){x = mean_prop_working(design = indv_svy,
+                                                            list_of_variables = indv_cols_to_analyse)
+
+    } else {x = mean_prop_working(design = indv_svy,
+                                  list_of_variables = indv_cols_to_analyse,
+                                  aggregation_level = groups[[i]])}
+    
+    x <- as.data.frame(t(x))
+    colnames(x) <- rep(group_names[i], ncol(x))
+    
+    # Assign the result to a tab. Number the tab as some cropped names are identical)
+    indv_results[[paste0(i, "_", substr(x = group_names[i], start=1, stop = 28))]] =  x
+    
+    # Paste the group names into the sheet:
+    if(i == length(group_names)){
+      indv_results[["groupings"]] = group_names
+      indv_results[["input data"]] = indv_svy$variables}
+  
+  }
+  
+  write.xlsx(indv_results, paste0("03 - Outputs/04_basic_analysis/", str_replace_all(Sys.Date(),"-","_"), "_indv_analysis.xlsx"), row.names = TRUE)
---
+++ 01 - Scripts/Care Givers Cleaning Script v3.R
@@ -0,0 +1,226 @@
+#---------------------------------
+# Education Sector - Care Givers
+# Data Cleaning Script
+#---------------------------------
+# Ben.Smith@reach.initiative.org
+# 06/12/2020
+#-----------
+
+
+# Preamble ----------------------------------------------------------------
+
+# For manipulating inputs
+library(dplyr)
+library(openxlsx)
+library(stringr)
+library(openxlsx)
+library(butteR)
+library(tidyr)
+
+# Choose whether you would like to write outputs:
+write_output <-c("yes","no")[1]
+
+# Set the directory:
+setwd("C:/Users/Ben SMITH/Documents/Bangladesh/Projects/Education Sector Assessment/")
+
+# Read in the raw data ----------------------------------------------------
+
+# Household Response Data:
+hh <- read.csv("02 - Inputs/Household Survey Responses - BGD2006_Education_Caregivers_Final - all_versions - english - 2020-12-06 - 08-54-30.csv", 
+               check.names =FALSE,
+               na.strings = c(""," ","NA","N/A"), 
+               stringsAsFactors = F) %>% rename("X_uuid" = "_uuid")
+
+# Individual Response Data:
+indv <- read.csv("02 - Inputs/Individual Survey Responses - BGD2006_Education_Caregivers_Final - all_versions - english - 2020-12-06 - 08-54-30.csv", 
+                 check.names =FALSE,
+                 na.strings = c("", " ", "NA", "N/A"), 
+                 stringsAsFactors = F) %>% rename("X_uuid" = "_submission__uuid")
+
+
+# The Cleaning log:
+cleaning_log <- read.xlsx("02 - Inputs/20201130_Data_cleaning_logbook_v4.xlsx",
+                          na.strings = c(""," ","NA","N/A"))
+# # hh uuid that need removing from indv:
+# indv_remove = c("5f70fe4f-dd06-49a5-9ff5-e1dc592d9409",
+#                 "f13fff68-9c18-47be-bab6-6d49926a09d9",
+#                 "1f4ceb38-e7d7-4459-842d-d7b847eb0ea1",
+#                 "694a95ad-90ea-4b79-a6c8-9b2ea113556d",
+#                 "90a788a8-d4f8-48e5-a1f3-1044408769fa",
+#                 "baaca48e-950a-4156-8522-cc1d5bfb06e9",
+#                 "872a0731-cb29-4587-919e-d8fc67d086df",
+#                 "0f17c571-ae3e-4d3c-802a-6726b19d15f1",
+#                 "da2cbd72-b118-4158-89de-9f3231ff753f")
+# 
+# # Print a list of these for manual editing of the cleaning log (now saved in V3)
+# for(i in 1:length(indv_remove)){
+#   print(indv_remove[i])
+#   print(indv$'_index'[indv$X_uuid %in% indv_remove[i]])
+#   print("_____________")
+# }
+
+
+# Data cleaning ------------------------------------------------------------
+
+# Clean the Household responses:
+# Filter the cleaning data to only household:
+cleaning_log_hh <- filter(cleaning_log, dataset_loop== "household" & change_type != "no_action")
+
+# # Check the cleaning log for potential issues:
+# View(check_cleaning_log(df = hh, 
+#                         df_uuid = "X_uuid", 
+#                         cl = cleaning_log_hh, 
+#                         cl_change_type_col ="change_type", 
+#                         cl_change_col ="question", 
+#                         cl_uuid = "uuid",
+#                         cl_new_val = "new_value"))
+
+# Change values according to the cleaning log:
+hh_clean_data <- implement_cleaning_log(df = hh,
+                                        df_uuid = "X_uuid",
+                                        cl = cleaning_log_hh,
+                                        cl_change_type_col ="change_type",
+                                        cl_change_col ="question",
+                                        cl_uuid = "uuid",
+                                        cl_new_val = "new_value")
+
+
+# Clean the individual responses:
+
+# Remove the "attend_education_camp/non_religious_learning_centre" response (as it does not really exist).
+indv$`attend_education_camp/non_religious_learning_centre` = NULL
+
+# Filter the data to only individual responses:
+cleaning_log_indv  <- filter(cleaning_log, dataset_loop== "individual" & change_type != "no_action")
+
+# # Check the cleaning log for potential issues:
+# View(check_cleaning_log(df = indv, 
+#                         df_uuid = "_index", 
+#                         cl = cleaning_log_indv, 
+#                         cl_change_type_col ="change_type", 
+#                         cl_change_col ="question", 
+#                         cl_uuid = "index_number/instance_name",
+#                         cl_new_val = "new_value"))
+
+# Change values according to the cleaning log:
+indv_clean_data <- implement_cleaning_log(df = indv,
+                                          df_uuid = "_index",
+                                          cl = cleaning_log_indv,
+                                          cl_change_type_col ="change_type",
+                                          cl_change_col ="question",
+                                          cl_uuid = "index_number/instance_name",
+                                          cl_new_val = "new_value")
+
+# Make level_of_education_camp uniform by removing " " and capitals:
+indv_clean_data$level_of_education_camp = gsub(pattern = " ",
+                                               replacement = "_",
+                                               x = tolower(indv_clean_data$level_of_education_camp))
+
+# Remove all responses that do not have consent:
+hh_clean_consent <- hh_clean_data %>% dplyr::filter(informed_consent == "yes")
+
+# Write cleaned data:
+if(write_output == "yes"){
+  
+  write.csv(hh_clean_consent, 
+            paste0("03 - Outputs/02_clean_data/",
+                   str_replace_all(Sys.Date(),"-","_"),
+                   "_hh_cleaned_data_with_consent.csv"))
+  
+  write.csv(hh_clean_data, 
+            paste0("03 - Outputs/02_clean_data/",
+                   str_replace_all(Sys.Date(),"-","_"),
+                   "_hh_cleaned_data_all.csv"))
+  
+  write.csv(indv_clean_data, 
+            paste0("03 - Outputs/02_clean_data/",
+                   str_replace_all(Sys.Date(),"-","_"),
+                   "_indv_cleaned_data.csv"))
+
+}
+
+
+# Create Composite Variables (individual data only) ------------------------
+
+# Set up to add columns:
+indv_clean_w_comp <- indv_clean_data %>% dplyr::mutate(
+  
+  # Add column of age group:
+  age_group = case_when(age_of_child %in% 3:10 ~ "age_group_3_10",
+                        age_of_child %in% 11:14 ~ "age_group_11_14",
+                        age_of_child %in% 15:18 ~ "age_group_15_18",
+                        age_of_child %in% 19:24 ~ "age_group_19_24"),
+  
+  # Add an age & gender column:
+  gender_age_group = case_when(girl_3_10 >0 ~ "girl_3_10",
+                               girl_11_14 >0 ~ "girl_11_14",
+                               girl_15_18 >0 ~ "girl_15_18",
+                               boy_3_10 >0 ~ "boy_3_10",
+                               boy_11_14 >0 ~ "boy_11_14",
+                               boy_15_18 >0 ~ "boy_15_18",
+                               girl_19_24 >0 ~ "girl_19_24",
+                               boy_19_24 >0 ~ "boy_19_24"),
+  
+  # Add a disabled seeing column:
+  disabled_seeing = if_else(
+    difficulties_with_seeing %in% c("lot_of_difficulty", "cannot_do_at_all"),
+    true = "yes", false = "no", missing = NULL),
+  
+  # Add a disabled hearing column:
+  disabled_hearing = if_else(
+    difficulties_with_hearing %in% c("lot_of_difficulty", "cannot_do_at_all"),
+    true = "yes", false = "no", missing = NULL),
+  
+  # Add a disabled climbing column:
+  disabled_climbing = if_else(
+    difficulties_with_climbing %in% c("lot_of_difficulty", "cannot_do_at_all"),
+    true = "yes", false = "no", missing = NULL),
+  
+  # Add a disabled remembering column:
+  disabled_remembering = if_else(
+    difficulity_remembering %in% c("lot_of_difficulty", "cannot_do_at_all"),
+    true = "yes", false = "no", missing = NULL),
+  
+  # Add a disabled self care column:
+  disabled_self_care = if_else(
+    difficulty_with_self_care %in% c("lot_of_difficulty", "cannot_do_at_all"),
+    true = "yes", false = "no", missing = NULL),
+  
+  # Add a disabled communication care column:
+  disabled_communication = if_else(
+    difficulity_communicating %in% c("lot_of_difficulty", "cannot_do_at_all"),
+    true = "yes", false = "no", missing = NULL),
+  
+  # Add a columns for when education level does not change:
+  education_level_promotion_unchanged = if_else(
+    education_receive_in_2019_camp == level_of_education_camp,
+    true = "yes", false = "no", missing = NULL)
+  
+)
+
+# Add in columns for the education level promotions:
+prom_catagories = paste0(indv_clean_w_comp[,"education_receive_in_2019_camp"], "_to_", indv_clean_w_comp[,"level_of_education_camp"])
+for(p in unlist(unique(prom_catagories))){indv_clean_w_comp[[paste0("education_level_promotion_",p)]] = c("no","yes")[(prom_catagories %in% p) +1]}
+
+
+# Add on the Upazila column from the hh data using UUID as a match:
+indv_clean_w_comp = 
+  left_join(x = indv_clean_w_comp, 
+            y = hh_clean_consent[,c("X_uuid", "upazila", "group_of_caregivers")],
+            by = "X_uuid")
+
+# Write Composite Indicators ----------------------------------------------
+
+if(write_output == "yes"){
+  
+  write.csv(x = indv_clean_w_comp,
+            file = paste0("03 - Outputs/03_composite_indicators_with_data/",
+                          "Individual data - cleaned data with composite indicators - ",
+                          str_replace_all(Sys.Date(),"-","_"), ".csv"))
+}
+
+
+
+# # Tidy the workspace:
+# rm(list=ls()[! ls() %in% c("calc_weight", "weighting_hc", "weighting_camp",
+#                            "hh_clean_consent", "indv_clean_w_comp", "kat_split_by_conjunction")])
